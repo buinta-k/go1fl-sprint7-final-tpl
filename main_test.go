f0c7d20c3fd11e07cb079c69412b87b68166b650
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-22 17:56:44 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -70,8 +70,13 @@ func TestCafeCount(t *testing.T) {
 
 		bodyStr := response.Body.String()
 
-
+		var gotCount int
+		if bodyStr != "" {
 			slice := strings.Split(bodyStr, ",")
+			gotCount = len(slice)
+		} else {
+			gotCount = 0
+		}
 
 		assert.Equal(t, v.want, gotCount)
 	}
